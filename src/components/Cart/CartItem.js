7b4051f04a40f4fce2d9027a0d54baccc2f3c2e9
Krishna Yallapanthula <krishna@Krishnas-MBP.columbus.rr.com>
2019-01-20 15:49:12 -0500

    paypal button added

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -4,20 +4,17 @@ export default function CartItem({item, value}) {
   const {id, title, img, price,total,count} = item;
   const {increment, decrement,removeItem } = value;
   return (
-    <div className="row my-2 text-capitalize text-center">
-      <div className="col-10 mx-auto col-lg-12">
+    <div className="row my-1 text-capitalize text-center">
+      <div className="col-10 mx-auto col-lg-2">
         <img src={img} style={{width:'5rem',height:'5rem'}} className="img-fluid" alt="produc" />
        </div>
-       <div className="col-10 mx-auto col-lg-12">
+       <div className="col-10 mx-auto col-lg-2">
        <span className="d-lg-none">product:</span>
        {title}
        </div>
-       <div className="col-10 mx-auto col-lg-12">
+       <div className="col-10 mx-auto col-lg-2">
        <span className="d-lg-none">price:</span>
        {price}
-       </div>
-       <div className="col-10 mx-auto col-lg-12">
-       
        </div>
        <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
        <div className="d-flex justify-content-center">
@@ -26,13 +23,13 @@ export default function CartItem({item, value}) {
          <span className="btn btn-black mx-1" onClick={()=>increment(id)}>+</span>
        </div>
        </div>
-       <div className="col-10 mx-auto col-lg-12">
+       <div className="col-10 mx-auto col-lg-2">
         <div className="cart-icon" onClick={()=> removeItem(id)}>
         <i className="fas fa-trash"></i>
         </div>
       
        </div>
-       <div className="col-10 mx-auto col-lg-12">
+       <div className="col-10 mx-auto col-lg-2">
       <strong>item total: $  {total}</strong>
       
        </div>
